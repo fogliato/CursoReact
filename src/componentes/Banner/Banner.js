3471a4edd0c031a5e7a5eaa6086341a9039f767e
Luiz Fogliato Junior <luiz.fogliato@g.globo>
2023-11-15 19:26:49 -0300

    Versão inicial do organo

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+import './Banner.css'
+
+export const  Banner = () => {
+    return (
+        <header className="banner">
+            <img src="/Imagens/banner.png" alt="Banner do titulo" />
+        </header>
+    );
+}
